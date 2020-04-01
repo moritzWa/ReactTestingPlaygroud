186c7dd64a5aaee7ca14fe7a189d755f28386c74
Moritz Wallawitsch <42035131+moritzWa@users.noreply.github.com>
2020-04-01 20:36:54 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactTestingPlaygroud
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default (App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">test</header>
+    </div>
+  );
+});
